996070e4b7f0fa35f91c0f5c1acaacf0db4cad56
Lilit Gevorgyan <lilitturbizinfo@gmail.com>
2023-01-16 17:37:46 +0100

    feat: adding authentication and create game methods

--- src/test/java/videogamedb/scriptfundamentals/VideoGameDb.java
+++ src/test/java/videogamedb/scriptfundamentals/VideoGameDb.java
@@ -2,6 +2,7 @@ package videogamedb.scriptfundamentals;
 
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
+import net.sf.saxon.om.Chain;
 
 import java.time.Duration;
 import java.util.List;
@@ -11,35 +12,37 @@ import static io.gatling.javaapi.http.HttpDsl.*;
 
 public class VideoGameDb extends Simulation {
 
-    private static ChainBuilder getAllVideoGames =
-            repeat(3).on(
-                    exec(http("Get all videogames")
-                    .get("/videogame")
-                    .check(status().not(400), status().not(500)))
-            );
-
-
-    private static ChainBuilder getSpecificGame =
-            repeat(5,"myCounter").on(
-                    exec(http("Get a specific game Id - #{myCounter}")
-                            .get("/videogame/#{myCounter}")
-                            .check(status().in(200, 210)))
-            );
-
     private HttpProtocolBuilder httpProtocol = http
             .baseUrl("https://www.videogamedb.uk/api")
-            .acceptHeader("application/json");
+            .acceptHeader("application/json")
+            .contentTypeHeader("application/json");
+
+    private static ChainBuilder authenticate =
+            exec(http("Authentication")
+            .post("/authenticate")
+            .body(StringBody("{\n" +
+                    "  \"password\": \"admin\",\n" +
+                    "  \"username\": \"admin\"\n" +
+                    "}"))
+            .check(jmesPath("token").saveAs("jwtToken"))
+            .check(status().is(200)));
+
+    private static ChainBuilder createNewGame =
+            exec(http("Create new game")
+                    .post("/videogame")
+                    .header("Authorization", "Bearer #{jwtToken}")
+                    .body(StringBody("{\n" +
+                            "  \"category\": \"Platform\",\n" +
+                            "  \"name\": \"Mario\",\n" +
+                            "  \"rating\": \"Mature\",\n" +
+                            "  \"releaseDate\": \"2012-05-04\",\n" +
+                            "  \"reviewScore\": 85\n" +
+                            "}"))
+                    .check(status().is(200)));
 
     private ScenarioBuilder scn = scenario("First Scenario")
-
-            .exec(getAllVideoGames)
-            .pause(5)
-            .exec(getSpecificGame)
-            .pause(1, 5)
-            .repeat(2).on(
-                    exec(getAllVideoGames)
-            );
-
+            .exec(authenticate)
+            .exec(createNewGame);
     {
         setUp(
                 scn.injectOpen(atOnceUsers(1))
