d6c84f349918b07b1fadeadb1642fcc4aa3933f0
Lilit Gevorgyan <lilitturbizinfo@gmail.com>
2023-01-16 16:53:41 +0100

    feat: refactor scenario requests into different methods

--- src/test/java/videogamedb/scriptfundamentals/VideoGameDb.java
+++ src/test/java/videogamedb/scriptfundamentals/VideoGameDb.java
@@ -11,41 +11,27 @@ import static io.gatling.javaapi.http.HttpDsl.*;
 
 public class VideoGameDb extends Simulation {
 
+    private static ChainBuilder getAllVideoGames =
+            exec(http("Get all videogames")
+                    .get("/videogame")
+                    .check(status().not(400), status().not(500)));
+
+    private static ChainBuilder getSpecificGame =
+            exec(http("Get a specific game")
+                    .get("/videogame/1")
+                    .check(status().in(200, 210)));
+
     private HttpProtocolBuilder httpProtocol = http
             .baseUrl("https://www.videogamedb.uk/api")
             .acceptHeader("application/json");
 
     private ScenarioBuilder scn = scenario("First Scenario")
 
-            .exec(http("Get a specific game - 1")
-                    .get("/videogame/1")
-                    .check(status().in(200,201,204))
-                    .check(jmesPath("name").is("Resident Evil 4")))
-            .pause(1,10)
-
-            .exec(http("Get all Games")
-                    .get("/videogame")
-                    .check(status().not(404), status().not(500))
-                    .check(jmesPath("[1].id").saveAs("gameId")))
-            .pause(Duration.ofMillis(4000))
-            .exec(
-                    session -> {
-                        System.out.println(session);
-                        System.out.println("gameId set to " + session.getString("gameId"));
-                        return session;
-                    }
-            )
-
-            .exec(http("Get a specific game - #{gameId}")
-                    .get("/videogame/#{gameId}")
-                    .check(jmesPath("name").is("Gran Turismo 3"))
-                    .check(bodyString().saveAs("responseBody")))
-            .exec(
-                    session -> {
-                        System.out.println("Response Body is " + session.getString("responseBody"));
-                        return session;
-                     }
-                    );
+            .exec(getAllVideoGames)
+            .pause(5)
+            .exec(getSpecificGame)
+            .pause(1, 5)
+            .exec(getAllVideoGames);
 
     {
         setUp(
